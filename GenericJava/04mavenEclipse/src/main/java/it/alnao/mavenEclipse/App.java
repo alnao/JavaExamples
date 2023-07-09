de9f9a4014fea9827a62b0a5eeb6b5b186992d9f
Alberto Nao <alnao@alnao.it>
2023-07-09 14:18:24 +0200

    04mavenEclipse con Log4j

--- GenericJava/04mavenEclipse/src/main/java/it/alnao/mavenEclipse/App.java
+++ GenericJava/04mavenEclipse/src/main/java/it/alnao/mavenEclipse/App.java
@@ -1,14 +1,16 @@
 package it.alnao.mavenEclipse;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class App {
+	private static final Logger logger = LogManager.getLogger(App.class);
+	
+    public static void main( String[] args ){
+    	BasicConfigurator.configure();
         System.out.println( "Hello World!" );
+        logger.info("Hello world");
         //set a breakpoint here to debug project
         System.out.println( "END!" );
     }
