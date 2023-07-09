73d2d0752c5dd7fd6441053e07ce12dea168273c
Alberto Nao <alnao@alnao.it>
2023-07-09 13:48:14 +0200

    04mavenEclipse

---
+++ GenericJava/04mavenEclipse/src/main/java/it/alnao/mavenEclipse/App.java
@@ -0,0 +1,15 @@
+package it.alnao.mavenEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //set a breakpoint here to debug project
+        System.out.println( "END!" );
+    }
+}
